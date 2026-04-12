6fd8abb3c79f56e62fed30ae8ac9e44ef292661e
Andrey Statkevskiy <andstatik@gmail.com>
2026-04-12 13:20:05 -0400

    created a new component - main sign in page, created style for it and buttons

--- client/Routes.js
+++ client/Routes.js
@@ -2,6 +2,7 @@ import React, {Component, Fragment} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
 import { Login, Signup } from './components/AuthForm';
+import SignInPage from './components/SignInPage'
 import HomeAssyTech from './components/HomeAssyTech';
 import {me} from './store'
 
@@ -25,7 +26,8 @@ class Routes extends Component {
           </Switch>
         ) : (
           <Switch>
-            <Route path='/' exact component={ Login } />
+            {/* <Route path='/' exact component={ Login } /> */}
+            <Route path='/' exact component={ SignInPage } />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
           </Switch>
