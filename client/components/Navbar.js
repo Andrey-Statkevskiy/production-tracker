de3e332b13a0e3e2232c44fbb8b0e6553c850cd1
Andrey Statkevskiy <andstatik@gmail.com>
2026-04-12 12:44:14 -0400

    made preparation before creating a new component

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -26,14 +26,14 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          {/* <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>Logout</a> */}
+          <Link to="/home">Home</Link>
+          <a href="#" onClick={handleClick}>Logout</a>
         </div>
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
-          {/* <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link> */}
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Sign Up</Link>
         </div>
       )}
       </div>
