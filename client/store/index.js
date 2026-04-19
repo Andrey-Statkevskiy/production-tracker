1cbc5280eb965e3ca68ec1085c13993cf4f4cba1
Andrey Statkevskiy <andstatik@gmail.com>
2026-04-19 16:24:08 -0400

    migrated to newest redux

--- client/store/index.js
+++ client/store/index.js
@@ -1,14 +1,37 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
-import {createLogger} from 'redux-logger'
-import thunkMiddleware from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+// import { createStore, combineReducers, applyMiddleware } from 'redux'
+// import { createLogger } from 'redux-logger'
+// // import thunkMiddleware from 'redux-thunk'
+// import { thunk } from 'redux-thunk'
+// import { composeWithDevTools } from '@redux-devtools/extension'
+// import auth from './auth'
+
+// const reducer = combineReducers({ auth })
+
+// const middleware = composeWithDevTools(
+//   applyMiddleware(
+//     thunk,
+//     createLogger({ collapsed: true })
+//   )
+// )
+
+// const store = createStore(reducer, middleware)
+
+// export default store
+// export * from './auth'
+
+import { configureStore } from '@reduxjs/toolkit'
+import { createLogger } from 'redux-logger'
 import auth from './auth'
 
-const reducer = combineReducers({ auth })
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
-)
-const store = createStore(reducer, middleware)
+const logger = createLogger({ collapsed: true })
+
+const store = configureStore({
+  reducer: {
+    auth
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(logger)
+})
 
 export default store
 export * from './auth'
