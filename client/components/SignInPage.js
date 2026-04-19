96987b85c512fa551a9cd26ac73ae739890df686
Andrey Statkevskiy <andstatik@gmail.com>
2026-04-19 15:17:41 -0400

    removed enzyme, upgraded babel, and made changes to the signin component, rendering a login form. Last checkpoint before a react upgrade

--- client/components/SignInPage.js
+++ client/components/SignInPage.js
@@ -1,81 +1,65 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {authenticate} from '../store'
+import {Login} from './AuthForm'
 
-/**
- * COMPONENT
- */
-const SignInPage = props => {
-  const {name, displayName, handleSubmit, error} = props
+export class SignInPage extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isSignInFormVisible: false,
+    }
+    this.isSignInFormVisibleToggle = this.isSignInFormVisibleToggle.bind(this)
+  }
 
-  return (
+  componentDidMount() {
+    console.log(React.version);
+  }
+
+  isSignInFormVisibleToggle() {
+    this.setState({ isSignInFormVisible: false })
+  }
+
+  handleClick(e) {
+    e.preventDefault()
+  }
+
+  // componentDidUpdate() {
+  //   console.log("this", this);
+  //   console.log("state", this.state);
+  //   console.log("props", this.props);
+  // }
+
+  render() {
+    return (
     <div className='signInContainer'>
         <h1 style={{ margin: '50px 0 150px 0' }}>Sign-in as...</h1>
         <div className='signInButtonContainer'>
-            <button className='blueButton'>Assembly Technician</button>
-            <button className='blueButton'>Production Leader</button>
-            <button className='blueButton'>TV</button>
+          {this.state.isSignInFormVisible ? (
+            (<button
+              type="button"
+              className="blueButton"
+              onClick={() => this.setState({ isSignInFormVisible: false })}
+            >
+              Assembly Technician
+            </button>)
+            &&
+            (<Login
+              isSignInFormVisible={this.isSignInFormVisibleToggle}
+            />)
+          ) : (
+            <button
+              type="button"
+              className="blueButton"
+              onClick={() => this.setState({ isSignInFormVisible: true })}
+            >
+              Assembly Technician
+            </button>
+          )}
+          <button className='blueButton'>Production Leader</button>
+          <button className='blueButton'>TV</button>
         </div>
-
-
-      <form onSubmit={handleSubmit} name={name}>
-        <div>
-          <label htmlFor="username">
-            <small>Username</small>
-          </label>
-          <input name="username" type="text" />
-        </div>
-        <div>
-          <label htmlFor="password">
-            <small>Password</small>
-          </label>
-          <input name="password" type="password" />
-        </div>
-        <div>
-          <button type="submit">{displayName}</button>
-        </div>
-        {error && error.response && <div> {error.response.data} </div>}
-      </form>
     </div>
   )
-}
-
-/**
- * CONTAINER
- *   Note that we have two different sets of 'mapStateToProps' functions -
- *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
- *   function, and share the same Component. This is a good example of how we
- *   can stay DRY with interfaces that are very similar to each other!
- */
-const mapLogin = state => {
-  return {
-    name: 'login',
-    displayName: 'Login',
-    error: state.auth.error
   }
 }
 
-const mapSignup = state => {
-  return {
-    name: 'signup',
-    displayName: 'Sign Up',
-    error: state.auth.error
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    handleSubmit(evt) {
-      evt.preventDefault()
-      const formName = evt.target.name
-      const username = evt.target.username.value
-      const password = evt.target.password.value
-      dispatch(authenticate(username, password, formName))
-    }
-  }
-}
-
-
-export default connect(mapLogin, mapDispatch)(SignInPage)
-// export const Login = connect(mapLogin, mapDispatch)(SignInPage)
-// export const Signup = connect(mapSignup, mapDispatch)(SignInPage)
+export default SignInPage;
