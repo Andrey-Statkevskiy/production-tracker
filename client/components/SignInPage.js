6e4e4420f1cf38f87d3f8da3193102dc7dbc9e64
Andrey Statkevskiy <andstatik@gmail.com>
2026-04-19 19:03:45 -0400

    Cleaned up the files. Sign in page is now tidy and is ready for optimal and responsive logging in for users

--- client/components/SignInPage.js
+++ client/components/SignInPage.js
@@ -1,65 +1,14 @@
-import React from 'react'
-import {Login} from './AuthForm'
-
-export class SignInPage extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      isSignInFormVisible: false,
-    }
-    this.isSignInFormVisibleToggle = this.isSignInFormVisibleToggle.bind(this)
-  }
-
-  componentDidMount() {
-    console.log(React.version);
-  }
-
-  isSignInFormVisibleToggle() {
-    this.setState({ isSignInFormVisible: false })
-  }
-
-  handleClick(e) {
-    e.preventDefault()
-  }
-
-  // componentDidUpdate() {
-  //   console.log("this", this);
-  //   console.log("state", this.state);
-  //   console.log("props", this.props);
-  // }
-
-  render() {
+export const SignInPage = () => {
     return (
     <div className='signInContainer'>
-        <h1 style={{ margin: '50px 0 150px 0' }}>Sign-in as...</h1>
-        <div className='signInButtonContainer'>
-          {this.state.isSignInFormVisible ? (
-            (<button
-              type="button"
-              className="blueButton"
-              onClick={() => this.setState({ isSignInFormVisible: false })}
-            >
-              Assembly Technician
-            </button>)
-            &&
-            (<Login
-              isSignInFormVisible={this.isSignInFormVisibleToggle}
-            />)
-          ) : (
-            <button
-              type="button"
-              className="blueButton"
-              onClick={() => this.setState({ isSignInFormVisible: true })}
-            >
-              Assembly Technician
-            </button>
-          )}
-          <button className='blueButton'>Production Leader</button>
-          <button className='blueButton'>TV</button>
-        </div>
+      <h1 style={{ margin: '50px 0 150px 0' }}>Sign-in as...</h1>
+      <div className='signInButtonContainer'>
+        <button type="button" className="blueButton">Assembly Technician</button>
+        <button className='blueButton'>Production Leader</button>
+        <button className='blueButton'>TV</button>
+      </div>
     </div>
   )
-  }
 }
 
 export default SignInPage;
--- client/store/index.js
+++ client/store/index.js
@@ -1,24 +1,3 @@
-// import { createStore, combineReducers, applyMiddleware } from 'redux'
-// import { createLogger } from 'redux-logger'
-// // import thunkMiddleware from 'redux-thunk'
-// import { thunk } from 'redux-thunk'
-// import { composeWithDevTools } from '@redux-devtools/extension'
-// import auth from './auth'
-
-// const reducer = combineReducers({ auth })
-
-// const middleware = composeWithDevTools(
-//   applyMiddleware(
-//     thunk,
-//     createLogger({ collapsed: true })
-//   )
-// )
-
-// const store = createStore(reducer, middleware)
-
-// export default store
-// export * from './auth'
-
 import { configureStore } from '@reduxjs/toolkit'
 import { createLogger } from 'redux-logger'
 import auth from './auth'
