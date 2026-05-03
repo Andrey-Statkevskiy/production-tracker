36751c0a04288bc703a473b9f9945d1cd722ae70
Andrey Statkevskiy <andstatik@gmail.com>
2026-05-03 12:22:10 -0400

    extended the pass change capability to all users' home dashboards

--- client/components/HomeTv.js
+++ client/components/HomeTv.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { useState }  from 'react'
 import {connect} from 'react-redux'
+import ChangePassPage from './ChangePassPage'
 
 /**
  * COMPONENT
  */
 export const HomeTv = props => {
   const {username} = props 
-
+  const [showChangePassForm, setShowChangePassForm] = useState(false);
+  const [changePassMsg, setChangePassMsg] = useState(null)
+  
+  const handleChangePass = (reason) => {
+    if (reason === 'pass changed') {
+    setChangePassMsg('Password changed successfully')
+  } else if (reason === 'pressed cancel') {
+    setChangePassMsg('Password change cancelled')
+  }
+    setTimeout(() => setChangePassMsg(null), 2000)
+    setShowChangePassForm(false)
+  }
   return (
     <div>
       <h3>Welcome, {username}</h3>
+
+      <button onClick={() => setShowChangePassForm(true)}>
+        Change Password
+      </button>
+      {showChangePassForm && (
+        <ChangePassPage
+          onAnyBtnClick={handleChangePass}
+        />
+      )}
+      {changePassMsg && <div>{changePassMsg}</div>}
     </div>
   )
 }
