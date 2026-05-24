cdad365d7b2f9a14197d13ceeea4aaee7a04fd7b
Andrey Statkevskiy <andstatik@gmail.com>
2026-05-24 12:30:50 -0400

    styled the startScreen component - flex layout, fonts, margins, etc.

--- client/components/TechnicianHelpers/StartScreen.js
+++ client/components/TechnicianHelpers/StartScreen.js
@@ -4,40 +4,39 @@ import { startSession } from '../../store/sessions'
 
 const StartScreen = () => {
   const dispatch = useDispatch()
-  const username = useSelector(state => state.auth.username)
 
-  const [station, setStation] = useState('1-1')
-  const [level, setLevel] = useState(1)
+  const [station, setStation] = useState('Station')
+  const [level, setLevel] = useState(0)
 
   const handleStart = () => {
     dispatch(startSession(station, level))
   }
 
   return (
-    <div>
-      <h2>Welcome, {username}</h2>
-      <h3>Start Work</h3>
+    <div className='startScreenContainer'>
+      <p className='startScreenTitle'>Please select your station and level for the run</p>
 
-      <div>
-        <label>Station:</label>
+      <div className='startScreenStationLevelDropdowns'>
         <select value={station} onChange={e => setStation(e.target.value)}>
+          <option key={0} value={'1-0'}>{'Station'}</option>
           {[...Array(10)].map((_, i) => (
             <option key={i} value={`1-${i + 1}`}>
               {`1-${i + 1}`}
             </option>
           ))}
         </select>
-      </div>
 
-      <div>
-        <label>Level:</label>
         <select value={level} onChange={e => setLevel(Number(e.target.value))}>
+          <option value={0}>Level</option>
           <option value={1}>Level 1</option>
           <option value={2}>Level 2</option>
         </select>
       </div>
 
-      <button onClick={handleStart}>Start</button>
+      <div>
+        <button className='startSceenStartBtn' onClick={handleStart}>Start Run</button>
+      </div>
+
     </div>
   )
 }
