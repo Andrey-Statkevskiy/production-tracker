b10c56c9f6dc660544faecd36f1c7c572fc0e260
Andrey Statkevskiy <andstatik@gmail.com>
2026-05-30 13:51:18 -0400

    final comments

--- client/components/TechnicianHelpers/StartScreen.js
+++ client/components/TechnicianHelpers/StartScreen.js
@@ -8,7 +8,7 @@ const StartScreen = () => {
   const [station, setStation] = useState('Station')
   const [level, setLevel] = useState(0)
   const [errMsg, setErrMsg] = useState('')
-  const availableStations = ['1-3', '1-5', '1-7', '1-9', '1-11'];
+  const availableStations = ['1-3', '1-5', '1-7', '1-9', '1-11']; //lvl1: 1-9, 1-11; lvl2: (1-3), 1-5, 1-7
   const handleStart = () => {
     if (station === 'Station' || level === 0) {
       setErrMsg('Select level/station and try again')
