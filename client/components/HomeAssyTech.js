591523d97e0acb29e5cbf7774fbe21fc7cc1c140
Andrey Statkevskiy <andstatik@gmail.com>
2026-05-31 11:28:53 -0400

    technicians dashboard has a control panel that is based on their permissions and is responsive with respect to current helper components

--- client/components/HomeAssyTech.js
+++ client/components/HomeAssyTech.js
@@ -1,60 +1,34 @@
-import React, { useState, useEffect } from 'react'
-import {connect} from 'react-redux'
-import ChangePassPage from './ChangePassPage'
-import TechnicianDashboard from './TechnicianHelpers/TechnicianDashboard'
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
+import TechnicianDashboard from "./TechnicianHelpers/TechnicianDashboard";
+import ControlPanel from "./ControlPanel";
+import "./TechnicianHelpers/HomeAssyTech.css"
 
-/**
- * COMPONENT
- */
-export const HomeAssyTech = props => {
-  const {username, activeSession } = props
-  const [showChangePassForm, setShowChangePassForm] = useState(false);
-  const [changePassMsg, setChangePassMsg] = useState(null)
-  const isTechWorking = !!activeSession
-    
-  const handleChangePass = (reason) => {
-    if (reason === 'pass changed') {
-    setChangePassMsg('Password changed successfully')
-  } else if (reason === 'pressed cancel') {
-    setChangePassMsg('Password change cancelled')
-  }
-    setTimeout(() => setChangePassMsg(null), 2000)
-    setShowChangePassForm(false)
-  }
+export const HomeAssyTech = ({ username, activeSession }) => {
+  const isTechWorking = !!activeSession;
 
-  useEffect(() => {
-    if (isTechWorking && showChangePassForm) {
-      setShowChangePassForm(false)
-    }
-  }, [isTechWorking])
+  // useEffect(() => {
+  //   if (isTechWorking && showChangePassForm) {
+  //     setShowChangePassForm(false)
+  //   }
+  // }, [isTechWorking])
 
   return (
-    <div>
-      {!isTechWorking && (
-        <button onClick={() => setShowChangePassForm(true)}>
-          Change Password
-        </button>
-      )}
-      {showChangePassForm && (
-        <ChangePassPage
-          onAnyBtnClick={handleChangePass}
-        />
-      )}
-      {changePassMsg && <div>{changePassMsg}</div>}
-
+    <div className="homeAssyTechContainer">
+      {!isTechWorking && <ControlPanel />}
       <TechnicianDashboard />
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapState = (state) => {
   return {
     username: state.auth.username,
-    activeSession: state.session.activeSession
-  }
-}
+    activeSession: state.session.activeSession,
+  };
+};
 
-export default connect(mapState)(HomeAssyTech)
+export default connect(mapState)(HomeAssyTech);
