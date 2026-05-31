c5f018ffed2fa3c660e4f6b547ab8def61c3266f
Andrey Statkevskiy <andstatik@gmail.com>
2026-05-31 09:47:48 -0400

    leaders have adaptive control panel

--- client/components/ControlPanel.js
+++ client/components/ControlPanel.js
@@ -1,9 +1,10 @@
 import "./LeaderHelpers/HomeLead.css";
 import React, { useState } from "react";
+import { connect } from "react-redux";
 import SignUpPage from "./SignUpPage";
 import ChangePassPage from "./ChangePassPage";
 
-export const ControlPanel = () => {
+export const ControlPanel = ({ role }) => {
   const [showNewUserForm, setShowNewUserForm] = useState(false);
   const [showChangePassForm, setShowChangePassForm] = useState(false);
   const [newUserMsg, setNewUserMsg] = useState(null);
@@ -31,36 +32,42 @@ export const ControlPanel = () => {
   return (
     <div className="controlsContainer">
       <p className="columnTitle">Controls</p>
-      <button
-        className="btn btn-blue"
-        onClick={() => console.log("Set Target Btn clicked")}
-      >
-        Set Targets
-      </button>
-      <button
-        className="btn btn-blue"
-        onClick={() => console.log("Reset Progress Btn clicked")}
-      >
-        Reset Progress
-      </button>
-      <button
-        className="btn btn-blue"
-        onClick={() => console.log("View All Users Btn clicked")}
-      >
-        View All Users
-      </button>
-      <button
-        className="btn btn-blue"
-        onClick={() => console.log("View/Export Data Btn clicked")}
-      >
-        View/Export Data
-      </button>
-      <button className="btn btn-blue" onClick={() => setShowNewUserForm(true)}>
-        Create user
-      </button>
-      {showNewUserForm && <SignUpPage onAnyBtnClick={handleNewUser} />}
-      {newUserMsg && <div>{newUserMsg}</div>}
-
+      {role === "leader" && (
+        <div className="leaderOnlyControls">
+          <button
+            className="btn btn-blue"
+            onClick={() => console.log("Set Targets Btn clicked")}
+          >
+            Set Targets
+          </button>
+          <button
+            className="btn btn-blue"
+            onClick={() => console.log("Reset Progress Btn clicked")}
+          >
+            Reset Progress
+          </button>
+          <button
+            className="btn btn-blue"
+            onClick={() => console.log("View All Users Btn clicked")}
+          >
+            View All Users
+          </button>
+          <button
+            className="btn btn-blue"
+            onClick={() => console.log("View/Export Data Btn clicked")}
+          >
+            View/Export Data
+          </button>
+          <button
+            className="btn btn-blue"
+            onClick={() => setShowNewUserForm(true)}
+          >
+            Create user
+          </button>
+          {showNewUserForm && <SignUpPage onAnyBtnClick={handleNewUser} />}
+          {newUserMsg && <div>{newUserMsg}</div>}
+        </div>
+      )}
       <button
         className="btn btn-blue"
         onClick={() => setShowChangePassForm(true)}
@@ -75,4 +82,10 @@ export const ControlPanel = () => {
   );
 };
 
-export default ControlPanel;
+const mapState = (state) => {
+  return {
+    role: state.auth?.role,
+  };
+};
+
+export default connect(mapState)(ControlPanel);
