c2f498a4f504b835a8f4aab01906c6060344fc31
Andrey Statkevskiy <andstatik@gmail.com>
2026-06-03 21:29:14 -0400

    leaders can reset progress

--- client/components/LeaderHelpers/ResetProgress.js
+++ client/components/LeaderHelpers/ResetProgress.js
@@ -1,11 +1,39 @@
-import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./HomeLead.css";
 
 export const ResetProgress = () => {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const delay = (ms) => new Promise(res => setTimeout(res, ms));
+
+  const handleReset = async () => {
+    try {
+      setLoading(true);
+
+      await delay(1500);
+
+      await axios.post(
+        "/api/targets/progress/reset",
+        {},
+        {
+          headers: {
+            Authorization: localStorage.getItem("token"),
+          },
+        }
+      );
+
+      navigate("/home");
+    } catch (err) {
+      console.error("Reset failed:", err);
+      alert("Failed to reset progress");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="resetProgressContainer">
       <p className="resetProgressTitle">
@@ -13,10 +41,19 @@ export const ResetProgress = () => {
         <br />
         want to reset progress?
       </p>
-      <button className="btn btn-blue" onClick={() => console.log("hit yes")}>  {/* Logic goes here */}
-        Yes
+
+      <button
+        className="btn btn-blue"
+        onClick={handleReset}
+        disabled={loading}
+      >
+        {loading ? "Resetting..." : "Yes"}
       </button>
-      <button className="btn btn-red" onClick={() => navigate("/home")}>
+
+      <button
+        className="btn btn-red"
+        onClick={() => navigate("/home")}
+      >
         Go Back
       </button>
     </div>
